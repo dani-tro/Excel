e4cca7973f0756fdea697152f015ec9ca1887f70
Daniel Tropolinov <57357538+dani-tro@users.noreply.github.com>
2022-06-13 22:09:44 +0300

    Defined class Cell

---
+++ Excel/Cell.h
@@ -0,0 +1,18 @@
+#ifndef __CELL_HPP
+#define __CELL_HPP
+
+#include "Libraries.h"
+#include "Basic_functions.h"
+
+class Cell
+{
+private:
+	virtual std::ostream& do_print(std::ostream&) const = 0;
+	virtual std::optional<float> do_get_value() const;
+public:
+	std::ostream& print(std::ostream& out = std::cout) const;
+	std::ostream& println(std::ostream& out = std::cout) const;
+	std::optional<float> get_value() const;
+};
+
+#endif 
