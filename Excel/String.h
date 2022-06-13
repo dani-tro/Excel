a7b4bda28375db9e14494d379347052cbf6c44b8
Daniel Tropolinov <57357538+dani-tro@users.noreply.github.com>
2022-06-13 22:54:05 +0300

    Declared class String

---
+++ Excel/String.h
@@ -0,0 +1,19 @@
+#ifndef __STRING_HPP
+#define __STRING_HPP
+
+#include "Cell.h"
+
+class String : public Cell
+{
+private:
+	std::string str;
+
+	std::ostream& do_print(std::ostream&) const override final;
+	std::optional<float> do_get_value() const override final;
+public:
+	String(const std::string&);
+	operator std::optional<float>() const;
+};
+
+#endif
+
