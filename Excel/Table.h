9b9a509e34be525c03d58e77914d14f0e5226d9e
Daniel Tropolinov <57357538+dani-tro@users.noreply.github.com>
2022-06-16 18:01:41 +0300

    Declared new functions

--- Excel/Table.h
+++ Excel/Table.h
@@ -2,14 +2,23 @@
 #define __TABLE_HPP
 
 #include "Cell.h"
+#include "String.h"
+#include "Int.h"
+#include "Float.h"
+#include "Formula.h"
+#include "Empty_cell.h"
 
 class Table
 {
 private:
 
 	static Table* instance;
-	std::vector<std::vector<Cell*> > cells;
+	std::vector< std::vector<Cell* > > cells;
+	std::vector<uint32_t> columns_max_length;
+	uint32_t columns = 0;
+	std::fstream file;
 	Table() = default;
+	void evaluate_the_cells();
 
 public:
 
@@ -18,8 +27,11 @@ public:
 	Table operator=(const Table&) = delete;
 	Table operator=(const Table&&) = delete;
 	std::optional<float> get_cell_value(uint32_t, uint32_t) const;
+	Cell* get_cell_ptr(uint32_t, uint32_t) const;
 	static Table* get_instance();
 	static void free();
+	void read_from_file();
+	void print(std::ostream&) const;
 
 };
 
