26494dcf680a719b3cb8e086bb33090ee65191d4
Daniel Tropolinov <57357538+dani-tro@users.noreply.github.com>
2022-06-20 16:35:05 +0300

    Declared class Command_executor

---
+++ Excel/Command_executor.h
@@ -0,0 +1,47 @@
+#ifndef __COMMAND_EXECUTOR
+#define __COMMAND_EXECUTOR
+
+#include "Basic_functions.h"
+#include "Command_line.h"
+#include "Table.h"
+
+constexpr uint32_t number_of_valid_commands = 8;
+const std::string valid_commands[number_of_valid_commands] = 
+{"open", "close", "save", "saveas", "help", "exit", "print", "edit"};
+constexpr uint32_t number_of_arguments_for_command[number_of_valid_commands] = { 1, 0, 0, 1, 0, 0, 0, 2 };
+
+class Command_executor
+{
+private:
+
+	static Command_executor* instance;
+	bool done = false;
+	bool is_there_a_loaded_file = false;
+	std::string loaded_file_name = "";
+
+	Command_executor() = default;
+	bool is_done() const;
+	void execute(const Command_line&);
+	bool is_valid(const Command_line&) const;
+
+	void open(const std::string&);
+	void close();
+	void save();
+	void saveas(const std::string&);
+	void help() const;
+	void exit();
+	void print() const;
+	void edit(uint32_t, uint32_t, const std::string&);
+
+public:
+	Command_executor(const Command_executor&) = delete;
+	Command_executor(const Command_executor&&) = delete;
+	Command_executor operator=(const Command_executor&) = delete;
+	Command_executor operator=(const Command_executor&&) = delete;
+	static Command_executor* get_instance();
+	static void free();
+	
+	void run();
+};
+
+#endif
