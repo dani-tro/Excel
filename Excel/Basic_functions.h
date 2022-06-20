822594e0fc84ae621f7b2bfc0e0c48cf1bc79c10
Daniel Tropolinov <57357538+dani-tro@users.noreply.github.com>
2022-06-20 23:40:57 +0300

    Declared new function

--- Excel/Basic_functions.h
+++ Excel/Basic_functions.h
@@ -6,13 +6,14 @@
 bool is_digit(char);
 int get_digit(char);
 float get_number(const std::string&, uint32_t&);
-float get_number(const std::string&, uint32_t&, char delimiter);
+float get_number(const std::string&, uint32_t&, char);
 bool is_operation(char);
 uint32_t priority(char);
 std::optional<float> perform_operation(float, char, float);
 std::pair<int, int> read_cell_indexes(const std::string&, uint32_t&);
 void tolower(std::string&);
 bool does_file_exist(const std::string&);
+void remove_spaces_at_the_ends(std::string&);
 
 #endif 
 
